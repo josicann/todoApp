1e5afc2061efcbad8e3b6d016e265394134092fc
josicann <josicann@ut.utm.edu>
2019-01-20 05:31:38 -0600

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.listen(3000, ()=> {
     console.log('started on port 3000');
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text:req.body.text
